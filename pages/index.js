edbe062797aef65f49ddaf06b4c6202261225d37
sansqra <sqrasan@gmail.com>
2021-01-08 16:48:33 +0530

    login/signup btns

--- pages/index.js
+++ pages/index.js
@@ -1,99 +1,50 @@
 import Head from 'next/head'
 
 import React from 'react';
-import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
-import Fab from '@material-ui/core/Fab';
-import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
-import Zoom from '@material-ui/core/Zoom';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import WbIncandescentIcon from '@material-ui/icons/WbIncandescent';
 
-import LaptopChromebookSharpIcon from '@material-ui/icons/LaptopChromebookSharp';
-import { Button } from '@material-ui/core';
+const theme = {
+  spacing: [0, 2, 3, 5, 8],
+}
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    position: 'fixed',
-    bottom: theme.spacing(2),
-    right: theme.spacing(2),
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
   },
   button: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    border: 0,
-    borderRadius: 3,
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
-    height: 48,
-    padding: '0 30px',
-  },
+    position: 'relative',
+    // alignSelf: "flex-end"
+  }
 }));
 
-function ScrollTop(props) {
-  const { children, window } = props;
+
+const Appbar = function () {
   const classes = useStyles();
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
-  const trigger = useScrollTrigger({
-    target: window ? window() : undefined,
-    disableHysteresis: true,
-    threshold: 100,
-  });
-
-  const handleClick = (event) => {
-    const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
-
-    if (anchor) {
-      anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
-    }
-  };
-
   return (
-    <Zoom in={trigger}>
-      <div onClick={handleClick} role="presentation" className={classes.root}>
-        {children}
-      </div>
-    </Zoom>
-  );
-}
-
-ScrollTop.propTypes = {
-  children: PropTypes.element.isRequired,
-  window: PropTypes.func,
-};
-
-function BackToTop(props) {
-  const classes = useStyles()
-  return (
-    <React.Fragment>
-      <CssBaseline />
-      <AppBar color='transparent'>
-        <Toolbar>
-          <Typography variant="h5" >
-            {/* <LaptopChromebookSharpIcon />
-            {'  '} */}
-            <span>GS-Suite</span>
+    <div>
+      <AppBar position="static" className={classes.root} color="transparent">
+        <Toolbar variant="regular">
+          <WbIncandescentIcon />
+          <Typography variant="h6" className={classes.title}>
+            GS-Suite
           </Typography>
+          <Button mt={1} variant="contained" color="primary" className={classes.button}>Login</Button>
+          <Button mt={1} variant="contained" color="primary" className={classes.button}>Sign Up</Button>
         </Toolbar>
-        <Button className={classes.button}>Login</Button>
       </AppBar>
-      <Toolbar id="back-to-top-anchor" />
-      <Container>
-        <Box my={2}>
-        </Box>
-      </Container>
-      <ScrollTop {...props}>
-        <Fab color="secondary" size="small" aria-label="scroll back to top">
-          <KeyboardArrowUpIcon />
-        </Fab>
-      </ScrollTop>
-    </React.Fragment>
+    </div>
   );
 }
 
@@ -105,7 +56,7 @@ export default function Home() {
         <title>GS-Suite</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <BackToTop />
+      <Appbar />
     </div >
   )
 }
